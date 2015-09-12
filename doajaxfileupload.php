b7a3d7da0348f6671830c6d9935d52f0612a714a
MD Rubyat Islam <rubyat.ais@gmail.com>
2015-09-12 10:02:06 +0600

    AjaxFileUploader
    Ajax File Uploader

---
+++ doajaxfileupload.php
@@ -0,0 +1,63 @@
+<?php
+	$error = "";
+	$msg = "";
+	$fileElementName = 'fileToUpload';
+	if(!empty($_FILES[$fileElementName]['error']))
+	{
+		switch($_FILES[$fileElementName]['error'])
+		{
+
+			case '1':
+				$error = 'The uploaded file exceeds the upload_max_filesize directive in php.ini';
+				break;
+			case '2':
+				$error = 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form';
+				break;
+			case '3':
+				$error = 'The uploaded file was only partially uploaded';
+				break;
+			case '4':
+				$error = 'No file was uploaded.';
+				break;
+
+			case '6':
+				$error = 'Missing a temporary folder';
+				break;
+			case '7':
+				$error = 'Failed to write file to disk';
+				break;
+			case '8':
+				$error = 'File upload stopped by extension';
+				break;
+			case '999':
+			default:
+				$error = 'No error code avaiable';
+		}
+	}elseif(empty($_FILES['fileToUpload']['tmp_name']) || $_FILES['fileToUpload']['tmp_name'] == 'none')
+	{
+		$error = 'No file was uploaded..';
+	}else 
+	{
+	
+	
+			$msg .= " File Name: " . $_FILES['fileToUpload']['name'] . ", ";
+			$msg .= " File Size: " . @filesize($_FILES['fileToUpload']['tmp_name']);
+			
+			$fileTmpLoc = $_FILES['fileToUpload']['tmp_name']; 
+			$path = "upload/".$_FILES['fileToUpload']['name'];
+			
+			if(move_uploaded_file($fileTmpLoc, $path))
+			{ $msg .= "upload is complete"; 
+			} 
+			else 
+			{ $msg .= "move_uploaded_file function failed"; } 
+
+
+			//for security reason, we force to remove all uploaded file
+			//@unlink($_FILES['fileToUpload']);		
+	}		
+	echo "{";
+	echo				"error: '" . $error . "',\n";
+	echo				"msg: '" . $msg . "'\n";
+	echo "}";
+?>
